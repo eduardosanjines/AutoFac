eedf834afad43c60468eed1cc682b4054e80b43a
EduardoSanjines <eduardosanjines@gmail.com>
2018-07-24 21:34:08 +0200

    AutoFac Examples - Log4Net and Constructor

---
+++ AutoFac_Xml/AutoFacXml/AutoServiceCallerImp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFacXml
+{
+    public class AutoServiceCallerImp : AutoServiceCaller
+    {
+        public AutoServiceCallerImp() { }
+
+        private AutoService bmwAutoService;
+        private AutoService hondaAutoService;
+        private AutoService fordAutoService; 
+        public AutoServiceCallerImp(AutoService bmwAutoService, AutoService hondaAutoService, AutoService fordAutoService)
+        {
+            this.bmwAutoService = bmwAutoService;
+            this.hondaAutoService = hondaAutoService;
+            this.fordAutoService = fordAutoService;
+        }
+
+        public void CallAutoService() {
+
+            bmwAutoService.GetService();
+
+            // get ford's auto service
+            fordAutoService.GetService();
+
+            // get honda's auto service
+            hondaAutoService.GetService();
+
+        }
+
+
+    }
+}
---
+++ AutoFac_Xml/AutoFacXml/Program.cs
@@ -0,0 +1,52 @@
+﻿using Autofac;
+using Autofac.Configuration;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AutoFacXml
+{
+    public class Program
+    {
+        public static IContainer Container { get;private set; }
+
+        static void Main(string[] args)
+        {
+            Configure();
+            Load();
+            Console.ReadKey();
+
+        }
+            public static void Configure()
+            {
+
+            var config = new ConfigurationBuilder();
+            config.AddXmlFile("AutoFac.xml");
+
+            var module = new ConfigurationModule(config.Build());
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule(module);
+            Container = builder.Build();
+
+            }
+
+        public static void Load() {
+
+            using (var scope = Container.BeginLifetimeScope()) {
+                
+                var bmw = scope.Resolve<BMWAutoService>();
+                var honda = scope.Resolve<HondaAutoService>();
+                var ford = scope.Resolve<FordAutoService>();
+
+                AutoServiceCallerImp servi = new AutoServiceCallerImp(bmw, honda, ford);
+                servi.CallAutoService();
+            }
+        }
+    }
+}
